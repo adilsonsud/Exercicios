271ac945748261033e4a68dc88c572584e1a61e2
Adilson Freire <adilsonsud@yahoo.com.br>
2023-07-04 00:50:31 -0300

    Utilizando Scanner
    Foi utilizado a biblioteca Sacanner para entrada de dados

--- ex01.java
+++ ex01.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 public class ex01{
     public static void main(String[] args) {
         System.out.println("Exercicio 01");
-        System.out.println("imprimindo segunda linha para o github");        
+        System.out.println("imprimindo segunda linha para o github");   
+         System.out.println("-----------------------------------------");
+        
+        Scanner n = new Scanner(System.in); 
+        System.out.print("Digite seu nome: ");   
+        String nome = n.nextLine();
+        System.out.println("-----------------------------------------");
+        System.out.println("Seu nome e : " + nome);
+        n.close();
+        System.out.println();
+         
     }
 }
