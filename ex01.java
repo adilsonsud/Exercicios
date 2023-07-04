1010e1b8052caa68111734794e46fe7486225b6b
Adilson Freire <adilsonsud@yahoo.com.br>
2023-07-04 00:03:34 -0300

    imprimindo segunda linha
    segunda linha foi impressa na console

--- ex01.java
+++ ex01.java
@@ -1,6 +1,6 @@
 public class ex01{
     public static void main(String[] args) {
         System.out.println("Exercicio 01");
-        
+        System.out.println("imprimindo segunda linha para o github");        
     }
 }
